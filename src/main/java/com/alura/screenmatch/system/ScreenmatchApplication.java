bf46258684361c051c1dca78817b775743f56a98
Vicenzo Freitas <forenglishlearnema@gmail.com>
2026-02-21 21:41:20 -0300

    troquei valor de int i para 0

--- src/main/java/com/alura/screenmatch/system/ScreenmatchApplication.java
+++ src/main/java/com/alura/screenmatch/system/ScreenmatchApplication.java
@@ -22,19 +22,27 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoApi();
 		var serie = "suits";
 		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=suits&Season=1&apikey=aace5842");
+
 		System.out.println(json);
+
 		converteDados conversor = new converteDados();
 		dadosSerie dados =  conversor.obterdados(json,dadosSerie.class);
+
 		System.out.println(dados);
+
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=aace5842");
 		DadosEp dadosEp = conversor.obterdados(json,DadosEp.class);
+
 		System.out.println(dadosEp);
+
 		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 0; i < dados.temporadas(); i++) {
+
+		for (int i = 1; i < dados.temporadas(); i++) {
 			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season="+i+"&apikey=aace5842");
 			DadosTemporada dadosTemporada = conversor.obterdados(json,DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
+
 		temporadas.forEach(System.out::println);
 	}
 }
